a7fab793f410d0d04082f4afcf004b5da7891e85
Richard-Watanabe <shoryuwatanabe124@gmail.com>
2021-06-29 11:33:14 -0700

    Completed javascript-data-model-exercise

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,143 @@
+var orderHistory = [
+  {
+    orderInfo: {
+      orderPlacedDate: 'August 4, 2020',
+      totalPrice: '$34.00',
+      customerName: 'JS Masher',
+      orderNumber: '114-3941689-8772232',
+      orderDetails: 'Link',
+      orderInvoice: 'Link'
+    },
+    orderStatus: [
+      {
+        status: 'Delivered',
+        date: 'Aug 8, 2020',
+        deliverMessage: null,
+        deliverMethod: null
+      }
+    ],
+    productInfo: [
+      {
+        name: 'JavaScript for impatient programmers',
+        author: 'Rauschmayer, Dr. Axel',
+        returnWindow: 'Return window closed on Sep 7, 2020',
+        subtotal: '$31.55',
+        buyItAgain: 'buttonlink',
+        viewItem: 'buttonlink'
+      }
+    ],
+    button: [
+      {
+        productReview: 'buttonlink'
+      }
+    ]
+  },
+  {
+    orderInfo: {
+      orderPlacedDate: 'July 19, 2020',
+      totalPrice: '$44.53',
+      customerName: 'JS Masher',
+      orderNumber: '113-9984268-1280257',
+      orderDetails: 'Link',
+      orderInvoice: 'Link'
+    },
+    orderStatus: [
+      {
+        status: 'Delivered',
+        date: 'Jul 20, 2020',
+        deliverMessage: 'Your Package was delivered.',
+        deliverMethod: 'It was handed directly to a resident.'
+      }
+    ],
+    productInfo: [
+      {
+        name: 'The Timeless Way of Building',
+        author: 'Alexander, Christopher',
+        returnWindow: 'Return window closed on Aug 19, 2020',
+        subtotal: '$41.33',
+        buyItAgain: 'buttonlink',
+        viewItem: 'buttonlink'
+      }
+    ],
+    button: [
+      {
+        productReview: 'buttonlink'
+      }
+    ]
+  },
+  {
+    orderInfo: {
+      orderPlacedDate: 'July 4, 2020',
+      totalPrice: '$17.22',
+      customerName: 'JS Masher',
+      orderNumber: '114-2875557-9059409',
+      orderDetails: 'Link',
+      orderInvoice: 'Link'
+    },
+    orderStatus: [
+      {
+        status: 'Delivered',
+        date: 'Jul 7, 2020',
+        deliverMessage: 'Your Package was delivered.',
+        deliverMethod: 'It was handed directly to a resident.'
+      }
+    ],
+    productInfo: [
+      {
+        name: 'Gamecube Controller Adapter',
+        author: null,
+        returnWindow: 'Return window closed on Aug 5, 2020',
+        subtotal: '$15.98',
+        buyItAgain: 'buttonlink',
+        viewItem: 'buttonlink'
+      }
+    ],
+    button: [
+      {
+        productReview: 'buttonlink'
+      }
+    ]
+  },
+  {
+    orderInfo: {
+      orderPlacedDate: 'July 3, 2020',
+      totalPrice: '$138.93',
+      customerName: 'JS Masher',
+      orderNumber: '113-2883177-2648248',
+      orderDetails: 'Link',
+      orderInvoice: 'Link'
+    },
+    orderStatus: [
+      {
+        status: 'Delivered',
+        date: 'Jul 5, 2020',
+        deliverMessage: null,
+        deliverMethod: null
+      }
+    ],
+    productInfo: [
+      {
+        name: 'Gamecube Controller',
+        author: null,
+        returnWindow: 'Return window closed on Aug 4, 2020',
+        subtotal: '$94.95',
+        buyItAgain: 'buttonlink',
+        viewItem: 'buttonlink'
+      },
+      {
+        name: 'The Art of Sql',
+        author: 'Faroult, Stephane',
+        returnWindow: 'Return window closed on Aug 4, 2020',
+        subtotal: '$33.99',
+        buyItAgain: 'buttonlink',
+        viewItem: 'buttonlink'
+      }
+    ],
+    button: [
+      {
+        productSupport: 'buttonlink',
+        productReview: 'buttonlink'
+      }
+    ]
+  }
+];
