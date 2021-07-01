9bb012b25ed1718c05b381e09abe57cbb94369ae
Richard-Watanabe <shoryuwatanabe124@gmail.com>
2021-07-01 16:47:28 -0700

    Used the strictly not equal operator to filter out strings

--- javascript-loops-practice/filter-out-strings.js
+++ javascript-loops-practice/filter-out-strings.js
@@ -3,7 +3,7 @@
 function filterOutStrings(values) {
   var filtered = [];
   for (var i = 0; i < values.length; i++) {
-    if (typeof values[i] === 'number') {
+    if (typeof values[i] !== 'string') {
       filtered.push(values[i]);
     }
   }
