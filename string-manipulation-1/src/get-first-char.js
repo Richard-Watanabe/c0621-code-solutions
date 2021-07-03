0e72119ea876cdaebe1a3e40cac241f51d96b882
Richard-Watanabe <shoryuwatanabe124@gmail.com>
2021-07-02 17:06:30 -0700

    Completed string-manipulation-1

---
+++ string-manipulation-1/src/get-first-char.js
@@ -0,0 +1,13 @@
+/* exported getFirstChar */
+
+// We need a storage to put the first character in.
+// We need to find the first character in the string and store it.
+// We need to return the first character of the string that was found.
+
+function getFirstChar(string) {
+  var firstCharacter = '';
+  firstCharacter = string[0];
+  return firstCharacter;
+}
+
+getFirstChar('Cody');
