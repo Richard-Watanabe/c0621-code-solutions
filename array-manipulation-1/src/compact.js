7d9b605b5310569db26083ecf63ca5152a47a529
Richard-Watanabe <shoryuwatanabe124@gmail.com>
2021-07-06 10:01:18 -0700

    Trying to make progress on last question

--- array-manipulation-1/src/compact.js
+++ array-manipulation-1/src/compact.js
@@ -3,9 +3,7 @@
 function compact(array) {
   var newArray = [];
   for (var i = 0; i < array.length; i++) {
-    if (array[i] !== false && array[i] !== 0 && array[i] !== undefined && array[i] !== '' && array[i] !== null) {
-      newArray.push(array[i]);
-    } else if (isNaN(array[i]) === true) {
+    if (array[i] !== false && array[i] !== 0 && array[i] !== undefined && array[i] !== '' && array[i] !== null && isNaN(array[i]) === false) {
       newArray.push(array[i]);
     }
   }
