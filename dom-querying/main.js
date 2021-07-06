64c30fefa9ed8888d93a8e665be0e3ecadc5190f
Richard-Watanabe <shoryuwatanabe124@gmail.com>
2021-07-06 10:28:51 -0700

    Completed dom-querying exercise

---
+++ dom-querying/main.js
@@ -0,0 +1,24 @@
+console.log('hello, world');
+
+var mainHeading = document.querySelector('h1');
+
+console.log(mainHeading);
+console.dir(mainHeading);
+
+var explanation = document.querySelector('#explanation');
+
+console.log(explanation);
+console.dir(explanation);
+
+var hint = document.querySelector('.hint');
+
+console.log(hint);
+console.dir(hint);
+
+var paragraphs = document.querySelectorAll('p');
+
+console.log(paragraphs);
+
+var exampleLink = document.querySelectorAll('.example-link');
+
+console.log(exampleLink);
