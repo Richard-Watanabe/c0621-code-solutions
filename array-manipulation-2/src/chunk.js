35e5ad8a4064d1fa042eb3cfa71d15dd95fde6cc
Richard-Watanabe <shoryuwatanabe124@gmail.com>
2021-07-06 14:40:47 -0700

    Completed array-manipulation-2

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,12 +1,16 @@
 /* exported chunk */
 
+// We need a storage for the new array.
+// We need to check each element in the array at chunks of the size parameter.
+// We need to get data amount of size, which will be new array data, and store that into the new array.
+// We need to return the content of the storage.
+
 function chunk(array, size) {
   var newArray = [];
-  var finalArray = [];
-  for (var i = 0; i < size; i++) {
-    newArray.push(array[i]);
+  for (var i = 0; i < array.length; i += size) {
+    newArray.push(array.slice(i, i + size));
   }
-  return finalArray;
+  return newArray;
 }
 
 chunk(['foo', 'bar', 'baz', 'qux'], 2);
