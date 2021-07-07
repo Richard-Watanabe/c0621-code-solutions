45a069bcfecb452cb4c43228da03324e8f2c5123
Richard-Watanabe <shoryuwatanabe124@gmail.com>
2021-07-07 10:00:27 -0700

    In progress, question 1

--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -1,1 +1,17 @@
 /* exported pick */
+
+// function pick(source, keys) {
+//   var newObject = {};
+//   for (var key in source) {
+//     for (var i = 0; i < keys.length; i++) {
+//       if (keys[i] === key) {
+//         newObject.key = keys[i];
+//       }
+//       return newObject;
+//     }
+//   }
+// }s
+
+// pick({ foo: 1, bar: 2, baz: 3 }, ['foo', 'baz']); // -> { foo: 1, baz: 3 }
+// pick({ qux: 4, corge: 5 }, ['bar', 'grault']); // -> {}
+// pick({ bar: 2 }, ['foo', 'bar', 'baz']); // -> { bar: 2 }
