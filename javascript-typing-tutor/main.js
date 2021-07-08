aabc63e2f19b2b166b150828aa064b5d5a14389e
Richard-Watanabe <shoryuwatanabe124@gmail.com>
2021-07-07 23:35:30 -0700

    In progress, made the document body listen for the keydown event

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,4 +1,5 @@
 var $letter = document.querySelectorAll('span');
+var $body = document.querySelector('body');
 
 function check(event) {
   for (var i = 0; i < $letter.length; i++) {
@@ -11,5 +12,5 @@ function check(event) {
 }
 
 for (var i = 0; i < $letter.length; i++) {
-  $letter[i].addEventListener('keydown', check);
+  $body.addEventListener('keydown', check);
 }
