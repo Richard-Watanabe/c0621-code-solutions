0b97766a7b09fbd1404753cb910dd953c8e47bdf
Richard-Watanabe <shoryuwatanabe124@gmail.com>
2021-07-08 09:57:20 -0700

    In progress

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,16 +1,26 @@
 var $letter = document.querySelectorAll('span');
 var $body = document.querySelector('body');
+var wordIndex = 0;
 
 function check(event) {
-  for (var i = 0; i < $letter.length; i++) {
-    if (event.key === $letter[i].textContent) {
-      $letter[i].className = 'green border-bottom';
+  while (wordIndex < $letter.length) {
+    if (event.key === $letter[wordIndex].textContent) {
+      $letter[wordIndex].className = 'green';
     } else {
-      $letter[i].className = 'red border-bottom';
+      $letter[wordIndex].className = 'red';
     }
+    wordIndex++;
   }
 }
 
 for (var i = 0; i < $letter.length; i++) {
   $body.addEventListener('keydown', check);
 }
+
+// for (var i = 0; i < $letter.length; i++) {
+//   if (event.key === $letter[i].textContent) {
+//     $letter[i].className = 'green space';
+//   } else {
+//     $letter[i].className = 'red space';
+//   }
+// }
