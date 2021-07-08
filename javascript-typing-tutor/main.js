30bb9c873c510dbecd71e43a2720a37237e68ee1
Richard-Watanabe <shoryuwatanabe124@gmail.com>
2021-07-08 14:29:57 -0700

    In progress

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -4,13 +4,11 @@ var letterIndex = 0;
 
 function check(event) {
   if (event.key === $letter[letterIndex].textContent) {
-    $letter[letterIndex].className = 'green';
+    $letter[letterIndex].className = 'green border-bottom';
     letterIndex++;
   } else {
     $letter[letterIndex].className = 'red border-bottom';
-    letterIndex++;
   }
-  $letter[letterIndex].className = 'border-bottom';
 }
 
 for (var i = 0; i < $letter.length; i++) {
