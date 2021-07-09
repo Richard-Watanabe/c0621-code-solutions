fc5282464027f469c03ca166a233b8d52de2e762
Richard-Watanabe <shoryuwatanabe124@gmail.com>
2021-07-08 21:56:02 -0700

    Moved the conditional out of the loop for first-chars function

--- string-manipulation-2/src/first-chars.js
+++ string-manipulation-2/src/first-chars.js
@@ -1,18 +1,18 @@
 /* exported firstChars */
 
 // Prepare a empty string as storage for the final output.
-// Check each letter in the string passed in.
-// Update the storage with the number letters of the length passed in.
 // If the length parameter is larger than the characters in the string, return the string as is.
-// If not, return the stored string.
+// If not, check each letter in the string passed in.
+// Update the storage with the number letters of the length passed in.
+// Return the stored string.
 
 function firstChars(length, string) {
   var newString = '';
+  if (string.length < length) {
+    return string;
+  }
   for (var i = 0; i < length; i++) {
     newString += string[i];
-    if (string.length < length) {
-      newString = string;
-    }
   }
   return newString;
 }
