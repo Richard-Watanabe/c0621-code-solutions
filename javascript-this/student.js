8a996b7ffbf3f45cc7a4b618e190935c6ab775dd
Richard-Watanabe <shoryuwatanabe124@gmail.com>
2021-07-19 11:47:34 -0700

    Completed javascript-this exercise

---
+++ javascript-this/student.js
@@ -0,0 +1,14 @@
+/* exported student */
+
+var student = {
+  firstName: 'Richard',
+  lastName: 'Watanabe',
+  subject: 'JavaScript',
+  getFullName: function () {
+    var fullName = this.firstName + ' ' + this.lastName;
+    return fullName;
+  },
+  getIntroduction: function () {
+    return 'Hello, my name is ' + this.firstName + ' ' + this.lastName + ' and I am studying ' + this.subject + '.';
+  }
+};
