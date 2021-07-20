8dbe58a535dde73c204bf34c900840c518964447
Richard-Watanabe <shoryuwatanabe124@gmail.com>
2021-07-19 21:33:56 -0700

    Completed object-manipulation-2

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,22 +1,12 @@
 /* exported defaults */
 
-// We need to check if the properties in source already exists in target or not.
-// If it doesn't add the property to target.
+// We need to check if the properties in source already exists in target or not, and also check if the value in source isn't null.
+// If it doesn't exist and if it isn't null, add to target.
 
 function defaults(target, source) {
   for (var property in source) {
-    if (property !== target[property]) {
+    if (!target[property] && target[property] !== null) {
       target[property] = source[property];
     }
   }
 }
-
-// for (var property in source) {
-//   var value = source[property];
-//   target[property] = value;
-//   for (var key in target) {
-//     if (key === property) {
-
-//     }
-//   }
-// }
