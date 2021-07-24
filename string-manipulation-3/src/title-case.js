c19f24369cf3073ade78128314bad45d3c73f046
Richard Watanabe <shoryuwatanabe124@gmail.com>
2021-07-24 23:00:40 +0000

    In progress, last question

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,1 +1,14 @@
 /* exported titleCase */
+
+// If a word is javascript, make it JavaScript.
+// If a word is api, make it API.
+// If a word is 3 letter or less and not API, keep it lower-case.
+// If it does not fit all of the above, capitilize the first letter.
+
+function titleCase(title) {
+
+}
+
+titleCase('composing software');
+
+// ['composing', 'software'];
