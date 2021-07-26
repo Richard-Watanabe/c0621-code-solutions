ca9908333407d5e5591b0742967efc8b837402fc
Richard Watanabe <shoryuwatanabe124@gmail.com>
2021-07-26 04:24:30 +0000

    In progress

--- array-manipulation-3/src/unique.js
+++ array-manipulation-3/src/unique.js
@@ -1,9 +1,16 @@
 /* exported unique */
 
-function unique(array) {
-  var uniqueArray = [];
-  for (var i = 0; i < array.length; i++) {
-    uniqueArray.push(array[i]);
-  }
+// Check every value in the array.
+// If the value isn't pushed into the newArray already, push it to the newArray.
+// If a value has already been pushed, don't push it into the newArray.
+// Return the newArray.
 
+function unique(array) {
+  var newArray = [];
+  for (var i = 0; i < array.length; i++) {
+    if (!newArray.includes(array[i])) {
+      newArray.push(array[i]);
+    }
+  }
+  return newArray;
 }
