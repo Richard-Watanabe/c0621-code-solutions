ed19a5e8f646e2573c39c780c949ea9b6da3cc93
Richard Watanabe <shoryuwatanabe124@gmail.com>
2021-08-04 18:31:05 +0000

    Created a collection of four objects that represents players. Each player has a unique name and a hand to hold cards in

---
+++ object-manipulation-3/main.js
@@ -0,0 +1,24 @@
+console.log('Lodash is loaded:', typeof _ !== 'undefined');
+
+var player1 = {
+  name: 'Cody',
+  hand: []
+};
+
+var player2 = {
+  name: 'Tim',
+  hand: []
+};
+
+var player3 = {
+  name: 'Cass',
+  hand: []
+};
+
+var player4 = {
+  name: 'TJ',
+  hand: []
+};
+
+var allPlayers = [player1, player2, player3, player4];
+console.log(allPlayers);
