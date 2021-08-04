3e920dc9e394ad0426ad2ef8f7ec02120528c580
Richard Watanabe <shoryuwatanabe124@gmail.com>
2021-08-04 04:49:32 +0000

    In progress, union

--- array-manipulation-3/src/union.js
+++ array-manipulation-3/src/union.js
@@ -1,1 +1,16 @@
 /* exported union */
+
+function union(first, second) {
+  var newArray = [];
+  for (var i = 0; i < first.length; i++) {
+    if (first[i] !== second[i]) {
+      newArray.push(first[i]);
+    }
+  }
+  for (var j = 0; j < second.length; j++) {
+    if (second[j] !== first[j]) {
+      newArray.push(second[j]);
+    }
+  }
+  return newArray;
+}
