5fbc6a0ef095d1750146ccfdeeb0e22f677af6ea
Richard Watanabe <shoryuwatanabe124@gmail.com>
2021-08-04 16:52:47 +0000

    Finished union

--- array-manipulation-3/src/union.js
+++ array-manipulation-3/src/union.js
@@ -1,14 +1,21 @@
 /* exported union */
 
+// Have a storage for the new array.
+// Check if the new array already has the values in the first array.
+// If not, add those values to the new array.
+// Check if the new array already has the values in the second array.
+// If not, add those values to the new array.
+// Return the storage.
+
 function union(first, second) {
   var newArray = [];
   for (var i = 0; i < first.length; i++) {
-    if (first[i] !== second[i]) {
+    if (!newArray.includes(first[i])) {
       newArray.push(first[i]);
     }
   }
   for (var j = 0; j < second.length; j++) {
-    if (second[j] !== first[j]) {
+    if (!newArray.includes(second[j])) {
       newArray.push(second[j]);
     }
   }
