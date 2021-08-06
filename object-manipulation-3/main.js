7baaf764da010b34dc6ad41cea7d3c89b24609ee
Richard Watanabe <shoryuwatanabe124@gmail.com>
2021-08-06 22:27:11 +0000

    Completed object-manipulation-3

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -9,35 +9,36 @@
 
 console.log('Lodash is loaded:', typeof _ !== 'undefined');
 
-// var player1 = {
-//   name: 'Cody',
-//   hand: [],
-//   point: 0
-// };
+var player1 = {
+  name: 'Cody',
+  hand: [],
+  point: 0
+};
 
-// var player2 = {
-//   name: 'Tim',
-//   hand: [],
-//   point: 0
-// };
+var player2 = {
+  name: 'Tim',
+  hand: [],
+  point: 0
+};
 
-// var player3 = {
-//   name: 'Cass',
-//   hand: [],
-//   point: 0
-// };
+var player3 = {
+  name: 'Cass',
+  hand: [],
+  point: 0
+};
 
-// var player4 = {
-//   name: 'TJ',
-//   hand: [],
-//   point: 0
-// };
+var player4 = {
+  name: 'TJ',
+  hand: [],
+  point: 0
+};
 
-// var allPlayers = [player1, player2, player3, player4];
+var allPlayers = [player1, player2, player3, player4];
 var deck = [];
 var rank = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'];
 var suit = ['clubs', 'diamonds', 'hearts', 'spades'];
 var value = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10];
+var first = 0;
 var winningpoints = 0;
 var winner;
 
@@ -48,10 +49,26 @@ for (var i = 0; i < suit.length; i++) {
   }
 }
 
-// function shuffle() {
-//   var random = _.random(0, 52));
-//   return random;
-// }
+function shuffle() {
+  var random = _.random(0, (deck.length - 1));
+  return random;
+}
+
+for (var k = 0; k < allPlayers.length; k++) {
+  for (var m = 0; m < 2; m++) {
+    first = shuffle();
+    allPlayers[k].hand.push(deck[first]);
+    allPlayers[k].point = allPlayers[k].point + deck[first].value;
+    _.remove(deck, deck[first]);
+  }
+}
+
+for (var l = 0; l < allPlayers.length; l++) {
+  if (allPlayers[l].point > winningpoints) {
+    winningpoints = allPlayers[l].point;
+    winner = allPlayers[l];
+  }
+}
 
 console.log('The winner is', winner.name);
 console.log('Points:', winningpoints);
