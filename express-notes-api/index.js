aa33f5bc7de6452c948f87de376807218c69095c
Richard Watanabe <shoryuwatanabe124@gmail.com>
2021-08-17 05:05:38 +0000

    Completed express-notes-api

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-const fs = require('fs');
 const dataObject = require('./data.json');
+const fs = require('fs');
 
 app.get('/api/notes', (req, res) => {
   const notesArray = [];
@@ -16,7 +16,7 @@ app.get('/api/notes/:id', (req, res) => {
   if (id < 1) {
     res.status(400).send({ error: 'id must be a positive integer' });
   } else if (dataObject.notes[id] === undefined) {
-    res.status(404).send({ error: `Cannot find note with id ${id}` });
+    res.status(404).send({ error: `cannot find note with id ${id}` });
   } else {
     res.status(200).json(dataObject.notes[id]);
   }
@@ -27,20 +27,16 @@ app.use(express.json());
 app.post('/api/notes', (req, res) => {
   const newEntry = req.body;
   if (newEntry.content === undefined) {
-    res.status(400).send({ error: 'Content is a required field' });
+    return res.status(400).send({ error: 'Content is a required field' });
   }
-  const newObject = {};
-  newObject.id = dataObject.nextId;
-  for (const key in newEntry) {
-    newObject[key] = newEntry[key];
-  }
-  dataObject.notes[dataObject.nextId] = newObject;
+  newEntry.id = dataObject.nextId;
+  dataObject.notes[dataObject.nextId] = newEntry;
   dataObject.nextId++;
   fs.writeFile('data.json', JSON.stringify(dataObject, null, 2), 'utf8', err => {
     if (err) {
       res.status(500).send({ error: 'An unexpected error occured' });
     } else {
-      res.status(201).send(newObject);
+      res.status(201).send(newEntry);
     }
   });
 });
@@ -48,16 +44,38 @@ app.post('/api/notes', (req, res) => {
 app.delete('/api/notes/:id', (req, res) => {
   const id = req.params.id;
   if (id < 1) {
-    res.status(400).send({ error: 'id must be a positive integer' });
+    return res.status(400).send({ error: 'id must be a positive integer' });
   } else if (dataObject.notes[id] === undefined) {
-    res.status(404).send({ error: `Cannot find note with id ${id}` });
+    return res.status(404).send({ error: `Cannot find note with id ${id}` });
+  } else {
+    delete dataObject.notes[id];
   }
-  delete dataObject.notes[id];
   fs.writeFile('data.json', JSON.stringify(dataObject, null, 2), 'utf8', err => {
     if (err) {
       res.status(500).send({ error: 'An unexpected error occured' });
     } else {
       res.status(204).send('');
+    }
+  });
+});
+
+app.put('/api/notes/:id', (req, res) => {
+  const id = req.params.id;
+  const newEntry = req.body;
+  if (id < 1) {
+    return res.status(400).send({ error: 'id must be a positive integer' });
+  } else if (newEntry.content === undefined) {
+    return res.status(400).send({ error: 'content is a required field' });
+  } else if (dataObject.notes[id] === undefined) {
+    return res.status(404).send({ error: `cannot find note with id ${id}` });
+  } else {
+    dataObject.notes[id].content = newEntry.content;
+  }
+  fs.writeFile('data.json', JSON.stringify(dataObject, null, 2), 'utf8', err => {
+    if (err) {
+      res.status(500).send({ error: 'An unexpected error occured' });
+    } else {
+      res.status(200).send(dataObject.notes[id]);
     }
   });
 });
