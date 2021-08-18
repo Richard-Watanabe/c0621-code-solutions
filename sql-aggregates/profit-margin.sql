6265f152e68486045d96668369592d75eb5c2517
Richard Watanabe <shoryuwatanabe124@gmail.com>
2021-08-18 23:02:14 +0000

    Couldn't figure out optional challenge

--- sql-aggregates/profit-margin.sql
+++
@@ -1,9 +0,0 @@
-select "title",
-      (sum("payments"."amount") - "replacementCost") as "totalProfit"
-  from "films"
-  join "inventory" using ("filmId")
-  join "rentals" using ("inventoryId")
-  join "payments" using ("rentalId")
-group by "films"."filmId"
-order by "totalProfit" desc
-limit 5;
