1536184536e50fae77c2d1b2c2e381c9fdc35efd
Richard Watanabe <shoryuwatanabe124@gmail.com>
2021-08-26 00:45:38 +0000

    Completed react-app-drawer-component

---
+++ react-app-drawer-component/src/app-drawer.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+class AppDrawer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false
+    };
+    this.handleClickOpen = this.handleClickOpen.bind(this);
+    this.handleClickClose = this.handleClickClose.bind(this);
+  }
+
+  handleClickOpen() {
+    this.setState({
+      isOpen: true
+    });
+  }
+
+  handleClickClose() {
+    this.setState({
+      isOpen: false
+    });
+  }
+
+  render() {
+    if (this.state.isOpen === false) {
+      return <i className="fas fa-bars" onClick={this.handleClickOpen}></i>;
+    } else {
+      return <div>
+               <div className="menu">
+                 <div className="overlay" onClick={this.handleClickClose}>
+                   <h2 className="menu-title margin-bottom inline-block">Menu</h2><br></br>
+                   <a href='#' className="margin-bottom cursor inline-block">About</a><br></br>
+                   <a href='#' className="margin-bottom cursor inline-block">Get Started</a><br></br>
+                   <a href='#' className="margin-bottom cursor inline-block">Sign In</a><br></br>
+                 </div>
+               </div>
+             </div>;
+    }
+  }
+}
+
+export default AppDrawer;
